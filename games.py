1d84d116505ad6a3a4eeaad41a305205d698e768
Tomás Sironi <sironitomas@gmail.com>
2020-07-17 15:08:07 -0300

    Adds board generation

--- games.py
+++ games.py
@@ -3,16 +3,33 @@ from random import randint
 
 class Minesweeper:
 
-    def __init__(self, rows=8, columns=8):
+    def __init__(self, rows=8, columns=8, mines=12):
         self.board = []
         self.rows = rows
         self.columns = columns
+        self.mines = mines
         self.size = self.rows * self.columns
-        for _ in range(rows):
+
+        mines_position = []
+        while len(mines_position) < self.mines:
+            x, y = randint(0, columns-1), randint(0, rows-1)
+            if (x, y) not in mines_position:
+                mines_position.append((x, y))
+
+        for _ in range(self.rows):
             row = []
-            for _ in range(columns):
-                if randint(0, rows*columns) < self.size / 5:
-                    row.append(-1)
-                else:
-                    row.append(0)
+            for _ in range(self.columns):
+                row.append(0)
             self.board.append(row)
+
+        # record the mines into the board
+        for x, y in mines_position:
+            self.board[y][x] = -1
+
+        # compute and record cells surrounding mines
+        for x, y in mines_position:
+            for dy in (-1, 0, 1):
+                for dx in (-1, 0, 1):
+                    if 0 <= x+dx < self.rows and 0 <= y+dy < self.columns:
+                        if self.board[y+dy][x+dx] != -1:
+                            self.board[y+dy][x+dx] += 1
